0212d95e6e91a1451f9c03f7a176a3fbb6b6ba80
Sławomir Król <skkrolu@gmail.com>
2023-11-09 11:33:28 +0100

    Supports EU URL for tokenization

--- AndroidSdk/src/main/java/com/recurly/androidsdk/data/network/core/RetrofitHelper.kt
+++ AndroidSdk/src/main/java/com/recurly/androidsdk/data/network/core/RetrofitHelper.kt
@@ -1,6 +1,6 @@
 package com.recurly.androidsdk.data.network.core
 
-import okhttp3.OkHttpClient
+import com.recurly.androidsdk.data.model.RecurlySessionData
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,10 +10,16 @@ internal object RetrofitHelper {
      * Builds the retrofit for its direct use
      * @return Retrofit
      */
-    fun getRetrofit(): Retrofit{
+    fun getRetrofit(): Retrofit {
+        val defaultURL = "https://api.recurly.com/"
+        val defaultURLEU = "https://api.eu.recurly.com/"
+
+        val baseUrl =
+            if (RecurlySessionData.getPublicKey().startsWith("fra-")) defaultURLEU else defaultURL
+
         return Retrofit.Builder()
             .client(OkHttpHelper.getOkHttpClient())
-            .baseUrl("https://api.recurly.com/")
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
